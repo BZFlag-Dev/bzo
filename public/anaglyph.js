0c66e7e122c36af1f94479965690feb65416e690
Tim Riker <timriker@gmail.com>
2025-12-12 11:30:53 -0700

    anaglyph, of course

---
+++ public/anaglyph.js
@@ -0,0 +1,4 @@
+// anaglyph.js
+// Provides a wrapper for Three.js AnaglyphEffect for use in render.js
+import { AnaglyphEffect } from 'three/addons/effects/AnaglyphEffect.js';
+export { AnaglyphEffect };
